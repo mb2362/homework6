5c96be57f92ad3071c5d05be5772090622c4d472
mb2362 <mb2362@njit.edu.com>
2025-03-03 19:44:11 -0500

    feat: added tests for .env

---
+++ tests/test_env.py
@@ -0,0 +1,11 @@
+"""Test for .env"""
+import pytest  # pylint: disable=unused-import
+from app import App
+
+def test_app_get_environment_variable():
+    """Test the 'get_environment_variable' method of the App class."""
+    app = App()
+    # Retrieve the current environment setting
+    current_env = app.get_environment_variable('ENVIRONMENT')
+    # Assert that the current environment is what you expect
+    assert current_env in ['DEVELOPMENT', 'TESTING', 'PRODUCTION'], f"Invalid ENVIRONMENT: {current_env}"
